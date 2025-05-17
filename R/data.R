bfa4dd793d655b03fa905654a0e87b6c15f0918e
manuarambburu <manugonzalezaramburu@gmail.com>
2025-05-17 18:38:43 +0200

    hopefully data is finally correctly added

--- R/data.R
+++ R/data.R
@@ -1,1 +1,12 @@
+#' Data to showcase the hole deployment of random lasso methodology over the cox model.
+#'
+#' Contains survival status of 70 individuals with their survival times and information about the log2 median count of a set of 700 genes.
+#' There are also some variablesa as sex, age and ECOG score.
+#'
+#' @format A data frame with 70 rows and 706 variables
+#'
+#' @source Data obtained from some hospital in Spain that has been full anonymaized.
+#'
+#' @examples
+#' data(myelasthoma)
 "myelasthoma"
